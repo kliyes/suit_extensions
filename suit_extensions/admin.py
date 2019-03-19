9596c8ae960804231998327463f822430811259f
kliyes <tom@kliyes.com>
2019-03-19 14:18:58 +0800

    custom UserModel admin

---
+++ suit_extensions/admin.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .mixins import ExtensionModelAdminMixin
+
+
+UserModel = get_user_model()
+
+
+class ExtendedUserAdmin(ExtensionModelAdminMixin, BaseUserAdmin):
+    pass
+
+
+admin.site.unregister(UserModel)  # unregister default admin first
+admin.site.register(UserModel, ExtendedUserAdmin)
