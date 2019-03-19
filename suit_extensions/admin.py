fa912c8830cc4b14047fae94638eb074f87a1456
kliyes <tom@kliyes.com>
2019-03-19 15:01:40 +0800

    custom Group admin

--- suit_extensions/admin.py
+++ suit_extensions/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.models import Group
 
 from .mixins import ExtensionModelAdminMixin
 
@@ -12,5 +14,12 @@ class ExtendedUserAdmin(ExtensionModelAdminMixin, BaseUserAdmin):
     pass
 
 
+class ExtendedGroupAdmin(ExtensionModelAdminMixin, BaseGroupAdmin):
+    pass
+
+
 admin.site.unregister(UserModel)  # unregister default admin first
+admin.site.unregister(Group)  # unregister default admin first
+
 admin.site.register(UserModel, ExtendedUserAdmin)
+admin.site.register(Group, ExtendedGroupAdmin)
